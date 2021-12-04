5fcfd8cb93e65956c1be9e475d9a4f91455b13eb
Jacob Gasper <jacobgasper@gmail.com>
2021-12-04 13:38:08 -0500

    tweaking code attempting to fix heroku issues

--- models/workout.js
+++ models/workout.js
@@ -45,8 +45,6 @@ WorkoutSchema.virtual("totalDuration").get(function() {
 })
 
 
-
-
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //REQUIRE
 const express = require("express");
 const mongoose = require("mongoose");
-//changed to port 3001, as I have issues connecting to 3000 in past.
-const PORT = process.env.PORT || 3001;
+//changed to port 3005, as I have issues connecting to 3000 in past.
+const PORT = process.env.PORT || 3005;
 
 const app = express();
 
@@ -13,11 +13,16 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-});
-// ADD ROUTES  
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+  
+
+}, () => 
+console.log("DB CONNECTION TEST")
+);
+
+
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
